4da14819e7f1626d458cd9afc903decaffcd48bb
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-25 20:37:16 -0400

    Finishing touches

--- basicscodelab/app/src/main/java/xyz/fjarm/basicscodelab/MainActivity.kt
+++ basicscodelab/app/src/main/java/xyz/fjarm/basicscodelab/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,8 +12,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,8 +30,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.fjarm.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -76,47 +80,40 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     var expanded by rememberSaveable {
         mutableStateOf(false)
     }
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) {
-            48.dp
-        } else {
-            0.dp
-        },
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        ),
-        label = "extraPadding",
-    )
 
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+    Card(
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(
             modifier = Modifier
-                .padding(24.dp),
+                .padding(24.dp)
+                .animateContentSize(),
         ) {
             Column(
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)),
+                    .weight(.9f),
             ) {
                 Text(text = "Hello")
                 Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
                     fontWeight = FontWeight.ExtraBold
                 ))
+                if (expanded) {
+                    Text(text = stringResource(id = R.string.lorem_ipsum))
+                }
             }
-            ElevatedButton(
+            IconButton(
                 modifier = Modifier
-                    .weight(0.9f),
+                    .weight(.1f),
                 onClick = {
                     expanded = !expanded
                 },
             ) {
-                Text(
-                    text = if (expanded) "Show less" else "Show more",
-                    textAlign = TextAlign.Center,
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = stringResource(
+                        if (expanded) R.string.show_less else R.string.show_more
+                    )
                 )
             }
         }
