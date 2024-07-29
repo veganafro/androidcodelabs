5ce4161f93f783acbe9ef16051ffddc2df2877ca
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-29 10:47:40 -0400

    MySoothe app - Scaffold

--- basiclayoutscodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ basiclayoutscodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -50,6 +50,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -308,6 +309,18 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 @Composable
 fun MySootheAppPortrait() {
     // Implement composable here
+    MySootheTheme {
+        Scaffold(
+            bottomBar = {
+                SootheBottomNavigation()
+            }
+        ) { padding: PaddingValues ->
+            HomeScreen(
+                modifier = Modifier
+                    .padding(padding),
+            )
+        }
+    }
 }
 
 // Step: Bottom navigation - Material
