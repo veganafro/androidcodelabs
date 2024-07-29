a2018063d6a851cc7d60a60c096252244ae4d28a
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-29 15:42:16 -0400

    MySoothe app - Window size

--- basiclayoutscodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ basiclayoutscodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -58,6 +58,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Compact
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -70,9 +74,10 @@ import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MySootheApp() }
+        setContent { MySootheApp(calculateWindowSizeClass(activity = this)) }
     }
 }
 
@@ -401,8 +406,16 @@ fun MySootheAppLandscape(){
 
 // Step: MySoothe App
 @Composable
-fun MySootheApp() {
+fun MySootheApp(windowSize: WindowSizeClass) {
     // Implement composable here
+    when (windowSize.widthSizeClass) {
+        Compact -> {
+            MySootheAppPortrait()
+        }
+        else -> {
+            MySootheAppLandscape()
+        }
+    }
 }
 
 private val alignYourBodyData = listOf(
