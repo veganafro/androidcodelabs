ca372b05703b7b431b8fc6fd127cb174dcbac288
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-29 20:34:04 -0400

    Memory in a composable function

--- basicstatecodelab/app/src/main/java/xyz/fjarm/basicstatecodelab/WaterCounter.kt
+++ basicstatecodelab/app/src/main/java/xyz/fjarm/basicstatecodelab/WaterCounter.kt
@@ -5,19 +5,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    var count by remember {
+        mutableStateOf(0)
+    }
     Column(
         modifier = modifier
             .padding(16.dp),
     ) {
         Text(
-            text = "You have had $count glasses",
+            text = "You have had ${count} glasses",
         )
         Button(
             modifier = Modifier
@@ -26,7 +32,9 @@ fun WaterCounter(modifier: Modifier = Modifier) {
                 count += 1
             }
         ) {
-
+            Text(
+                text = "Add one",
+            )
         }
     }
 }
