336c1a3554e23501cca3764ec641143f8a5222e9
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-29 20:20:49 -0400

    Events in compose

--- basicstatecodelab/app/src/main/java/xyz/fjarm/basicstatecodelab/WaterCounter.kt
+++ basicstatecodelab/app/src/main/java/xyz/fjarm/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package xyz.fjarm.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,12 +11,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
+    var count = 0
+    Column(
         modifier = modifier
             .padding(16.dp),
-        text = "You have had $count glasses",
-    )
+    ) {
+        Text(
+            text = "You have had $count glasses",
+        )
+        Button(
+            modifier = Modifier
+                .padding(top = 8.dp),
+            onClick = {
+                count += 1
+            }
+        ) {
+
+        }
+    }
 }
 
 @Preview
