f06111a55e8b19946e5faae8cf1065faccf161e3
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-30 10:06:52 -0400

    Restore state in compose

--- basicstatecodelab/app/src/main/java/xyz/fjarm/basicstatecodelab/WaterCounter.kt
+++ basicstatecodelab/app/src/main/java/xyz/fjarm/basicstatecodelab/WaterCounter.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +19,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         modifier = modifier
             .padding(16.dp),
     ) {
-        var count by remember {
+        var count by rememberSaveable {
             mutableStateOf(0)
         }
         if (count > 0) {
